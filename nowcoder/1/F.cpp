e702c60cadcde46d1ed9b732dc5c6e6310355eea
Strive-for-excellence <1505656319@qq.com>
2018-09-16 00:49:10 +0800

    Update F.cpp

--- nowcoder/1/F.cpp
+++ nowcoder/1/F.cpp
@@ -1,1 +1,126 @@
 
+```
+#include <bits/stdc++.h>
+#define mem(ar,num) memset(ar,num,sizeof(ar))
+#define me(ar) memset(ar,0,sizeof(ar))
+#define lowbit(x) (x&(-x))
+#define Pb push_back
+#define  FI first
+#define  SE second
+#define For(i,a,b) for(int i = a; i < b; ++i)
+using namespace std;
+typedef long long LL;
+typedef unsigned long long ULL;
+const int    prime = 999983;
+const int    INF = 0x7FFFFFFF;
+const LL     INFF =0x7FFFFFFFFFFFFFFF;
+const double pi = acos(-1.0);
+const double inf = 1e18;
+const double eps = 1e-6;
+const LL     mod = 1e9 + 7;
+typedef LL ll ;
+int dr[2][4] = {1,-1,0,0,0,0,-1,1};
+typedef pair<int,int> P;
+LL qpow(LL a,LL b){
+	LL ans = 1;
+	while( b > 0){
+		 if(b & 1) ans = ans*a%mod;
+		 a = a*a%mod;
+		 b >>= 1;
+	}
+	return ans;
+} 
+namespace polysum {
+    #define rep(i,a,n) for (int i=a;i<n;i++)
+    #define per(i,a,n) for (int i=n-1;i>=a;i--)
+    const int D=2010;
+    ll a[D],f[D],g[D],p[D],p1[D],p2[D],b[D],h[D][2],C[D];
+    ll powmod(ll a,ll b){ll res=1;a%=mod;assert(b>=0);for(;b;b>>=1){if(b&1)res=res*a%mod;a=a*a%mod;}return res;}
+    ll calcn(int d,ll *a,ll n) { // a[0].. a[d]  a[n] 
+        if (n<=d) return a[n];
+        p1[0]=p2[0]=1;
+        rep(i,0,d+1) {
+            ll t=(n-i+mod)%mod;
+            p1[i+1]=p1[i]*t%mod;
+        }
+        rep(i,0,d+1) {
+            ll t=(n-d+i+mod)%mod;
+            p2[i+1]=p2[i]*t%mod;
+        }
+        ll ans=0;
+        rep(i,0,d+1) {
+            ll t=g[i]*g[d-i]%mod*p1[i]%mod*p2[d-i]%mod*a[i]%mod;
+            if ((d-i)&1) ans=(ans-t+mod)%mod;
+            else ans=(ans+t)%mod;
+        }
+        return ans;
+    }
+    void init(int M) {
+        f[0]=f[1]=g[0]=g[1]=1;
+        rep(i,2,M+5) f[i]=f[i-1]*i%mod;
+        g[M+4]=powmod(f[M+4],mod-2);
+        per(i,1,M+4) g[i]=g[i+1]*(i+1)%mod;
+    }
+    ll polysum(ll m,ll *a,ll n) { // a[0].. a[m] \sum_{i=0}^{n-1} a[i]
+        ll b[D];
+        for(int i=0;i<=m;i++) b[i]=a[i];
+        b[m+1]=calcn(m,b,m+1);
+        rep(i,1,m+2) b[i]=(b[i-1]+b[i])%mod;
+        return calcn(m+1,b,n-1);
+    }
+    ll qpolysum(ll R,ll n,ll *a,ll m) { // a[0].. a[m] \sum_{i=0}^{n-1} a[i]*R^i
+        if (R==1) return polysum(n,a,m);
+        a[m+1]=calcn(m,a,m+1);
+        ll r=powmod(R,mod-2),p3=0,p4=0,c,ans;
+        h[0][0]=0;h[0][1]=1;
+        rep(i,1,m+2) {
+            h[i][0]=(h[i-1][0]+a[i-1])*r%mod;
+            h[i][1]=h[i-1][1]*r%mod;
+        }
+        rep(i,0,m+2) {
+            ll t=g[i]*g[m+1-i]%mod;
+            if (i&1) p3=((p3-h[i][0]*t)%mod+mod)%mod,p4=((p4-h[i][1]*t)%mod+mod)%mod;
+            else p3=(p3+h[i][0]*t)%mod,p4=(p4+h[i][1]*t)%mod;
+        }
+        c=powmod(p4,mod-2)*(mod-p3)%mod;
+        rep(i,0,m+2) h[i][0]=(h[i][0]+h[i][1]*c)%mod;
+        rep(i,0,m+2) C[i]=h[i][0];
+        ans=(calcn(m,C,n)*powmod(R,n)-c)%mod;
+        if (ans<0) ans+=mod;
+        return ans;
+    }
+} // polysum::init();
+const int maxn = 1000+100;
+LL a[maxn],b[maxn];
+int main(void)
+{	
+   int n;
+   polysum::init(maxn);
+   while(scanf("%d",&n) != EOF){
+   	me(a),me(b);
+   	
+   	for(int i = 1;i <= n;  ++i)
+   	   scanf("%d",&a[i]);
+   	sort(a+1,a+n+1);
+   	LL now = 1;
+   	LL ans = 0;
+   	for(LL i = 1;i <= n;  ++i){
+   		if(a[i] == a[i-1]){
+   			now = now*a[i]%mod;
+   			continue;
+		}
+		   b[0] = 0;
+   			for(LL j = 1;j <= n-i+1;j++)
+   			   b[j] = j*(qpow(j,n-i+1)-qpow(j-1,n-i+1))%mod;
+		 ans += now*( polysum::polysum(n-i+1,b,a[i]+1)-polysum::polysum(n-i+1,b,a[i-1]+1))%mod;
+		 now = now*a[i]%mod;
+	   }
+	ans = (ans%mod+mod)%mod;
+	printf("%lld\n",ans);
+   }
+   return 0;
+}
+
+
+
+```
